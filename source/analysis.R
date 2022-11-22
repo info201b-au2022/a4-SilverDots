aa9a8b2c455a0a43217610292fffc47fdb93643e
SilverDots <RTran904@gmail.com>
2022-11-21 23:30:00 -0800

    Finish sec 4

--- source/analysis.R
+++ source/analysis.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(dplyr)
 library(stringr)
+library(ggplot2)
+library(scales)
 rm(list = ls())
 
 # The functions might be useful for A4
@@ -11,6 +13,9 @@ incarceration_df <- get_data()
 
 ## Obtain the column names of the dataset.
 features <- colnames(incarceration_df)
+
+## Load state names and codes for later use.
+state_codes <- read.csv("../source/state_names_and_codes.csv")
 
 ## Section 2  ----
 #----------------------------------------------------------------------------#
@@ -101,36 +106,113 @@ counties_highest_avg_prison_rate <- incarceration_df %>%
 
 ## Section 3  ----
 #----------------------------------------------------------------------------#
-# Growth of the U.S. Prison Population
-# This section contains functions responsible for data wrangling and charting
-# the growth of the U.S. prison population.
+# Growth of the U.S. Jail Population
+# This section contains functions for wrangling data to chart
+# the growth of the U.S. Jail population.
 #----------------------------------------------------------------------------#
-# Organizes 
+# Creates a tibble which organizes the total U.S. jail population by year.
+#
+# Returns:
+#   A tibble mapping years to the total U.S. jail populations for those years
 get_year_jail_pop <- function() {
   jail_pop_df <- incarceration_df %>%
     group_by(year) %>%
-    summarize(total_pop = sum(total_jail_pop, na.rm = TRUE))
+    summarize(total_pop = round(sum(total_jail_pop, na.rm = TRUE)))
   return(jail_pop_df)
 }
 
-# This function ... <todo:  update comment>
+# Generates an aesthetic plot of total U.S. jail population by year.
+#
+# Returns:
+#   A plot of the total U.S. jail population by year for the range of years
+#   from 1970-2018
 plot_jail_pop_for_us <- function()  {
-  # TODO: Implement this function
-  return()
-} 
+  plot_data <- get_year_jail_pop()
+  jail_plot <- ggplot(data = plot_data) +
+    geom_col(
+      mapping = aes(
+        x = year,
+        y = total_pop,
+        
+      )
+    ) +
+    scale_y_continuous(labels = label_comma()) +
+    labs(
+      title = "Increase of Jail Population in the U.S. (1970-2018)",
+      caption = "The U.S. jail population has seen a steady increase since
+                 1980, peaking in 2007",
+      x = "Year",
+      y = "Total Jail Population"
+    )
+  return(jail_plot)
+}
 
 ## Section 4  ----
 #----------------------------------------------------------------------------#
-# Growth of Prison Population by State
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
+# Growth of Jail Population by State
+# This section contains functions for wrangling data to chart the growth
+# of the U.S. jail population by state.
 #----------------------------------------------------------------------------#
+# Creates a tibble consisting of data on U.S. jail populations for the
+# provided vector of states.
+#
+# Arguments:
+#   states: a vector of state codes which represents the states whose data for
+#           jail population sizes are to be collected
+# Returns:
+#   A tibble mapping the given states to their total jail populations for
+#   the range of years from 1970-2018
+get_jail_pop_by_states <- function(states) {
+  jail_pop_by_states <- incarceration_df %>%
+    filter(state %in% states) %>%
+    group_by(state, year) %>%
+    summarize(total_pop = sum(total_jail_pop, na.rm = TRUE),
+              .groups = "drop") %>%
+    mutate("Code" = state) %>%
+    left_join(state_codes, by = "Code")
+  return(jail_pop_by_states)
+}
+
+# Generates a line chart of the total jail populations for the given states
+# from 1970-2018.
+#
+# Arguments:
+#   states: a vector of state codes which represents the states to include in
+#           the line chart
+# Returns:
+#   A line chart displaying the total jail populations for each given state
+#   for the range of years from 1970-2018\
+plot_jail_pop_by_states <- function(states) {
+  if (length(states) > 10) {
+    print("Number of states must be less than 10")
+    return(null)
+  }
+  plot_data <- get_jail_pop_by_states(states)
+  state_pop_plot <- ggplot(data = plot_data,
+                           mapping = aes(
+                             x = year,
+                             y = total_pop,
+                             color = State
+                           )) +
+    geom_point() +
+    geom_smooth() +
+    scale_y_continuous(labels = label_comma()) +
+    labs(
+      title = "Increase of U.S. Jail Population by State (1970-2018)",
+      x = "Year",
+      y = "Total Jail Population",
+      caption = "Jail populations for states of various sizes are shown to
+      capture how jail population counts vary by population size"
+    )
+  return(state_pop_plot)
+}
 
 ## Section 5  ----
 #----------------------------------------------------------------------------#
-# <variable comparison that reveals potential patterns of inequality>
-# Your functions might go here ... <todo:  update comment>
-# See Canvas
+# Changes in Incarceration Rates by Gender Across Western States
+# The following section contains data wrangling functions for analyzing how
+# gender disparities in the incarceration rates of counties in the Western
+# United States.
 #----------------------------------------------------------------------------#
 
 ## Section 6  ----
